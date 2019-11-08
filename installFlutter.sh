7e5384ccaa4957040079f06e73a0f7397b4e5617
Vinicius Espindola <vinnyaoe@gmail.com>
2019-11-08 13:35:31 -0400

    fix export PATH

--- installFlutter.sh
+++ installFlutter.sh
@@ -8,7 +8,7 @@ cd /tmp
 sudo wget  -P /tmp -O flutterSDK.tar.xz $flutterSDK
 sudo tar xvf  /tmp/flutterSDK.tar.xz
 sudo mv flutter /opt/flutterSDK
-cd /home/vinny
-sudo echo "export \$PATH=\$PATH:/opt/flutter/bin" >> .zshrc
+cd $HOME
+sudo echo "export PATH=\$PATH:/opt/flutterSDK/bin" >> .zshrc
 
 
